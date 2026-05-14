d3b00f574c309afc7a1c43865d9dc8e638e1626f
argelapps-dotcom <argel.apps@gmail.com>
2026-05-14 16:15:40 +0800

    Update tracks.ts

--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -6,16 +6,16 @@ export const tracks: Track[] = [
     id: '1',
     title: 'Padayon Lang',
     artist: 'ArgelYee Echolab',
-    album: 'Synthetic Dreams',
+    album: 'Padayon Lang',
     coverUrl: 'https://cdn2.suno.ai/1ee17f39-4ac9-4f47-802c-d0d09e42d22b.jpeg?width=360',
     audioUrl: 'https://cdn1.suno.ai/7314013f-e394-4fad-9866-a36edd7e92d4.mp3',
-    duration: '3:15'
+    duration: '3:14'
   },
   {
     id: '2',
     title: 'Sakripisyo',
     artist: 'ArgelYee Echolab',
-    album: 'Synthetic Dreams',
+    album: 'Padayon Lang',
     coverUrl: 'https://cdn2.suno.ai/1ee17f39-4ac9-4f47-802c-d0d09e42d22b.jpeg?width=360',
     audioUrl: 'https://cdn1.suno.ai/6805c391-7f37-46c3-b7a0-843c921a5c8d.mp3',
     duration: '3:37'
@@ -24,7 +24,7 @@ export const tracks: Track[] = [
     id: '3',
     title: 'Salig ug Gugma',
     artist: 'ArgelYee Echolab',
-    album: 'Synthetic Dreams',
+    album: 'Padayon Lang',
     coverUrl: 'https://cdn2.suno.ai/1ee17f39-4ac9-4f47-802c-d0d09e42d22b.jpeg?width=360',
     audioUrl: 'https://cdn1.suno.ai/38d46d7d-d869-4c7f-8414-d4584ac2e158.mp3',
     duration: '2:43'
@@ -33,7 +33,7 @@ export const tracks: Track[] = [
     id: '4',
     title: 'Lisod Pero Padayon',
     artist: 'ArgelYee Echolab',
-    album: 'Synthetic Dreams',
+    album: 'Padayon Lang',
     coverUrl: 'https://cdn2.suno.ai/1ee17f39-4ac9-4f47-802c-d0d09e42d22b.jpeg?width=360',
     audioUrl: 'https://cdn1.suno.ai/4d1b0baa-fd56-4026-a3ee-df54fbb3ceaa.mp3',
     duration: '3:18'
