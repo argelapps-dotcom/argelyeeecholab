d4ca45e994c6c1612ce32269848cd4505689d82a
argelapps-dotcom <argel.apps@gmail.com>
2026-05-14 19:50:45 +0800

    Update tracks.ts

--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -7,7 +7,7 @@ export const tracks: Track[] = [
     title: 'Padayon Lang',
     artist: 'ArgelYee Echolab',
     album: 'Padayon Lang',
-    coverUrl: 'https://cdn2.suno.ai/1ee17f39-4ac9-4f47-802c-d0d09e42d22b.jpeg?width=360',
+    coverUrl: 'https://6a05a51246fc04f7c2e24501.imgix.net/Orange%20Sunset%20Aesthetic%20Music%20Album%20Cover.jpg',
     audioUrl: 'https://cdn1.suno.ai/7314013f-e394-4fad-9866-a36edd7e92d4.mp3',
     duration: '3:14'
   },
@@ -16,7 +16,7 @@ export const tracks: Track[] = [
     title: 'Sakripisyo',
     artist: 'ArgelYee Echolab',
     album: 'Padayon Lang',
-    coverUrl: 'https://cdn2.suno.ai/1ee17f39-4ac9-4f47-802c-d0d09e42d22b.jpeg?width=360',
+    coverUrl: 'https://6a05a51246fc04f7c2e24501.imgix.net/Orange%20Sunset%20Aesthetic%20Music%20Album%20Cover.jpg',
     audioUrl: 'https://cdn1.suno.ai/6805c391-7f37-46c3-b7a0-843c921a5c8d.mp3',
     duration: '3:37'
   },
@@ -25,7 +25,7 @@ export const tracks: Track[] = [
     title: 'Salig ug Gugma',
     artist: 'ArgelYee Echolab',
     album: 'Padayon Lang',
-    coverUrl: 'https://cdn2.suno.ai/1ee17f39-4ac9-4f47-802c-d0d09e42d22b.jpeg?width=360',
+    coverUrl: 'https://6a05a51246fc04f7c2e24501.imgix.net/Orange%20Sunset%20Aesthetic%20Music%20Album%20Cover.jpg',
     audioUrl: 'https://cdn1.suno.ai/38d46d7d-d869-4c7f-8414-d4584ac2e158.mp3',
     duration: '2:43'
   },
@@ -34,7 +34,7 @@ export const tracks: Track[] = [
     title: 'Lisod Pero Padayon',
     artist: 'ArgelYee Echolab',
     album: 'Padayon Lang',
-    coverUrl: 'https://cdn2.suno.ai/1ee17f39-4ac9-4f47-802c-d0d09e42d22b.jpeg?width=360',
+    coverUrl: 'https://6a05a51246fc04f7c2e24501.imgix.net/Orange%20Sunset%20Aesthetic%20Music%20Album%20Cover.jpg',
     audioUrl: 'https://cdn1.suno.ai/4d1b0baa-fd56-4026-a3ee-df54fbb3ceaa.mp3',
     duration: '3:18'
   },
@@ -43,7 +43,7 @@ export const tracks: Track[] = [
     title: 'Kaya Kaayo',
     artist: 'ArgelYee Echolab',
     album: 'Padayon Lang',
-    coverUrl: 'https://cdn2.suno.ai/1ee17f39-4ac9-4f47-802c-d0d09e42d22b.jpeg?width=360',
+    coverUrl: 'https://6a05a51246fc04f7c2e24501.imgix.net/Orange%20Sunset%20Aesthetic%20Music%20Album%20Cover.jpg',
     audioUrl: 'https://cdn1.suno.ai/40ef1ebe-dad1-47df-803b-f755c62c6332.mp3',
     duration: '3:05'  
   },
@@ -52,7 +52,7 @@ export const tracks: Track[] = [
     title: 'Kayod Lang',
     artist: 'ArgelYee Echolab',
     album: 'Padayon Lang',
-    coverUrl: 'https://cdn2.suno.ai/1ee17f39-4ac9-4f47-802c-d0d09e42d22b.jpeg?width=360',
+    coverUrl: 'https://6a05a51246fc04f7c2e24501.imgix.net/Orange%20Sunset%20Aesthetic%20Music%20Album%20Cover.jpg',
     audioUrl: 'https://cdn1.suno.ai/1425a01d-1ff7-4625-bf91-af652b90b35a.mp3',
     duration: '3:10' 
    }
