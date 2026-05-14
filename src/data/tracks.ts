8ee3b938d905083abddb101b90977e0e56bd2aa5
argelapps-dotcom <argel.apps@gmail.com>
2026-05-14 15:26:39 +0800

    Update tracks.ts

--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -22,20 +22,20 @@ export const tracks: Track[] = [
   },
   {
     id: '3',
-    title: 'Digital Rain',
-    artist: 'Echo Waves',
-    album: 'Singles',
+    title: 'Padayon Lang',
+    artist: 'ArgelYee Echolab',
+    album: 'adayon Lang',
     coverUrl: 'https://images.unsplash.com/photo-1518818419601-129561133e06?auto=format&fit=crop&q=80&w=500',
-    audioUrl: 'https://cdn1.suno.ai/6805c391-7f37-46c3-b7a0-843c921a5c8d.mp3',
-    duration: '3:37'
+    audioUrl: 'https://cdn1.suno.ai/7314013f-e394-4fad-9866-a36edd7e92d4.mp3',
+    duration: '3:15'
   },
   {
     id: '4',
-    title: 'Cybernetic Pulse',
-    artist: 'Echo Waves',
-    album: 'Singles',
+    title: 'Sakripisyo',
+    artist: 'ArgelYee Echolab',
+    album: 'adayon Lang',
     coverUrl: 'https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?auto=format&fit=crop&q=80&w=500',
-    audioUrl: 'https://cdn1.suno.ai/7314013f-e394-4fad-9866-a36edd7e92d4.mp3',
-    duration: '3:15'
+    audioUrl: 'https://cdn1.suno.ai/6805c391-7f37-46c3-b7a0-843c921a5c8d.mp3',
+    duration: '3:37'
   }
 ];
